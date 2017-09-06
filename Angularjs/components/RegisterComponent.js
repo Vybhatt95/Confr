84b3dc89b6366d7a19fe7e7aed957e3c5aa27c3e
Jeremy Wagner <jeremywag@gmail.com>
2017-09-06 14:20:00 -0400

    Back button isnt associated with form and added matching password functionality

--- Angularjs/components/RegisterComponent.js
+++ Angularjs/components/RegisterComponent.js
@@ -1,37 +1,37 @@
 var app = angular.module('app');
 
 app.component('register',{ 
-
-
+    
         template: '../views/register.html',
-        controller: function(){
-            $scope.user;
-            $scope.firstname;
-            $scope.lastname;
-            $scope.username;
-            $scope.password;
-            $scope.confirm;
-            $scope.email;
-            
-            var createUser = function(){
-                $http({
-                    url: '.../register',
-                    method: 'POST',
-                    headers: {'Content-Type' : 'application/json'},
-                    data: {
-                        "firstname": $scope.username,
-                        "lastname":  $scope.lastname,
-                        "username": $scope.username,
-                        "password": $scope.password,
-                        "email": $scope.email
-                    }
-                }).then(function(response){
-
-                }, function(response){
-
-                })
-            }
-        }
-
-
+        controller: RegisterController
 })
+
+function RegisterController(){
+    var ctrl = this;
+    ctrl.user;
+    ctrl.firstname;
+    ctrl.lastname;
+    ctrl.username;
+    ctrl.password;
+    ctrl.confirm;
+    ctrl.email;
+    
+    ctrl.createUser = function(){
+        $http({
+            url: '.../register',
+            method: 'POST',
+            headers: {'Content-Type' : 'application/json'},
+            data: {
+                "firstname": ctrl.username,
+                "lastname":  ctrl.lastname,
+                "username": ctrl.username,
+                "password": ctrl.password,
+                "email": ctrl.email
+            }
+        }).then(function(response){
+
+        }, function(response){
+
+        })
+    }
+}
