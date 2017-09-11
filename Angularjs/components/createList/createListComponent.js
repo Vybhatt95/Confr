437519c37aa925c7209ea0fbe394004f410a64b3
Vybhatt951 <Vybhatt95@gmail.com>
2017-09-11 16:24:01 -0400

    Fixing drop down for list alter/addition

---
+++ Angularjs/components/createList/createListComponent.js
@@ -0,0 +1,13 @@
+var app = angular.module('app');
+
+app.component('createListSw', {
+    templateUrl: 'components/analysis/createList-sw.html',
+    controller: createListController
+})
+
+function createListController(){
+console.log("Hello")
+
+
+
+}
--- Angularjs/components/listdetail/ListDetailComponent.js
+++ Angularjs/components/listdetail/ListDetailComponent.js
@@ -1,76 +1,3 @@
-var dummy1 = [
-    [{
-        "id": 1,
-        "itemname": "Asteraceae",
-        "price": "$29.03",
-        "barcode": "8296170396"
-      }, {
-        "id": 2,
-        "itemname": "Ranunculaceae",
-        "price": "$33.48",
-        "barcode": "5516658047"
-      }, {
-        "id": 3,
-        "itemname": "Brassicaceae",
-        "price": "$25.92",
-        "barcode": "1107791359"
-      }, {
-        "id": 4,
-        "itemname": "Myrtaceae",
-        "price": "$20.69",
-        "barcode": "6011580750"
-      }, {
-        "id": 5,
-        "itemname": "Brassicaceae",
-        "price": "$28.30",
-        "barcode": "3270816669"
-      }],
-
-      [{
-        "id": 1,
-        "itemname": "Asteraceae",
-        "price": "$3.49",
-        "barcode": "7385624723",
-        "crossedOff": false
-      }, {
-        "id": 2,
-        "itemname": "Cyperaceae",
-        "price": "$6.48",
-        "barcode": "5412777457",
-        "crossedOff": false
-      }, {
-        "id": 3,
-        "itemname": "Commelinaceae",
-        "price": "$1.32",
-        "barcode": "7652533465",
-        "crossedOff": false
-      }, {
-        "id": 4,
-        "itemname": "Cyperaceae",
-        "price": "$32.85",
-        "barcode": "8602284179",
-        "crossedOff": false
-      }, {
-        "id": 5,
-        "itemname": "Polygonaceae",
-        "price": "$13.04",
-        "barcode": "4326063297",
-        "crossedOff": false
-      }, {
-        "id": 6,
-        "itemname": "Polygonaceae",
-        "price": "$5.45",
-        "barcode": "8132264991",
-        "crossedOff": false
-      }, {
-        "id": 7,
-        "itemname": "Pertusariaceae",
-        "price": "$24.49",
-        "barcode": "4095665262",
-        "crossedOff": false
-      }]
-]
-
 var app = angular.module('app');
 
 app.component('listdetail',{
@@ -78,16 +5,44 @@ app.component('listdetail',{
     controller: ListDetailController
 })
 
-function ListDetailController(listObj){
+function ListDetailController(){
     var ctrl = this;
-    ctrl.listtitle = listObj.getObj().listname
-    
-    ctrl.list = dummy1[listObj.getObj().id];
-    //ctrl.crossedOff = false;
-    ctrl.crossOff = function(i){
 
-          i.crossedOff = !i.crossedOff
-      
+
+    ctrl.products = [{
+      "itemname": "Milk"
+    },
+    {
+      "itemname": "Apple"
+    },
+    {
+      "itemname": "Ice-Cream"
+    },
+    {
+      "itemname": "Nuts"
+    }]
+
+    //extra list to add to autocomplete
+    ctrl.productsToAdd = [{
+      "itemname": "Mango"
+    },
+    {
+      "itemname": "Carrot"
+    },
+    {
+      "itemname": "Peanut Butter"
+    },
+    {
+      "itemname": "Jelly"
+    }]
+    ctrl.listtitle = "Trial";
+    console.log(ctrl.listtitle);
+    // ctrl.products =["Milk", "Bread", "Cheese"];
+    ctrl.addItem = function(val){
+        console.log(ctrl.val);
+        ctrl.products.push({"itemname":ctrl.val});
 
     }
+
+
 }
