60c8ae78704298215d634f6b4d1440e775178271
Vybhatt951 <Vybhatt95@gmail.com>
2017-09-12 09:42:53 -0400

    prototype createlist done

--- Angularjs/components/createList/createListComponent.js
+++ Angularjs/components/createList/createListComponent.js
@@ -1,13 +1,32 @@
 var app = angular.module('app');
 
-app.component('createListSw', {
-    templateUrl: 'components/analysis/createList-sw.html',
+app.component('createlistSw', {
+    templateUrl: 'components/createList/createlist-sw.html',
     controller: createListController
 })
 
 function createListController(){
-console.log("Hello")
+      var ctrl = this;
 
+      ctrl.products = []
 
+      ctrl.productsToAdd = [{
+        "itemname": "Mango"
+      },
+      {
+        "itemname": "Carrot"
+      },
+      {
+        "itemname": "Peanut Butter"
+      },
+      {
+        "itemname": "Jelly"
+      }]
+
+      ctrl.addItem = function(val){
+          console.log(ctrl.val);
+          ctrl.products.push({"itemname":ctrl.val});
+
+      }
 
 }
