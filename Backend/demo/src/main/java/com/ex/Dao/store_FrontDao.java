f7b9f5b9d594c4bf1e706c5fe6e2d3767d6936db
Jeremy Wagner <jeremywag@gmail.com>
2017-09-12 22:11:07 -0400

    httpsession passes user info to home controller

--- Backend/demo/src/main/java/com/ex/Dao/store_FrontDao.java
+++ Backend/demo/src/main/java/com/ex/Dao/store_FrontDao.java
@@ -1,9 +1,8 @@
 package com.ex.Dao;
 
+
 import com.ex.Objects.Store_Front;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface store_frontDao extends JpaRepository<Store_Front, Integer> {
+public interface store_FrontDao extends JpaRepository<Store_Front,Integer> {
 }
