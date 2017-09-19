dc422f86a3e3bc07cb6699d815574403e81891c7
Vybhatt951 <Vybhatt95@gmail.com>
2017-09-18 22:45:12 -0400

    add more to the list

--- Backend/demo/src/main/resources/static/components/listdetail/ListDetailComponent.js
+++ Backend/demo/src/main/resources/static/components/listdetail/ListDetailComponent.js
@@ -5,7 +5,7 @@ app.component('listdetail',{
     controller: ListDetailController
 })
 
-function ListDetailController(ListService,ItemService, $routeParams){
+function ListDetailController(ListService,ItemService, $routeParams, $http){
     var ctrl = this;
     ctrl.itemList = ItemService.itemNames;
     ctrl.storeList = ItemService.storeNames;
@@ -24,13 +24,94 @@ function ListDetailController(ListService,ItemService, $routeParams){
       console.log(ctrl.list[$routeParams.index].items[$routeParams.index]);
       // console.log(ctrl.list[$routeParams.index].items[$routeParams.index].stores);
 
+      ctrl.$onInit = function() {
+        console.log("Hello");
+
+        ctrl.itemList = ItemService.itemNames;
+
+        ctrl.storeList = ItemService.storeNames;
+
+        if(ItemService.inventory.length <= 0) {
+          console.log("Sending Request");
+          $http({
+            url: "http://localhost:8080/item/all",
+            method: "GET",
+            headers: {'Content-Type' : 'application/json'}
+          }).then(function(response) {
+            ItemService.storeInventory(response.data);
+            console.log("All Items Processed");
+          }), function() {
+            console.log("This method is a failure");
+          }
+        }
+      }
 
 
-    ctrl.addItem = function(val){
-        console.log(ctrl.val);
-        ctrl.products.push({"itemname":ctrl.val});
+            ctrl.hasChanged = function (itemID){
+              console.log(itemID);
+                ctrl.priceStore=[];
+              for(var i =0; i<ctrl.storeList.length; i++){
+              ctrl.priceStore.push({"storeName": ctrl.storeList[i], "itemPrice" : ItemService.getPrice(itemID, ctrl.storeList[i])} );
 
-    }
+              }
+
+              console.log(ctrl.priceStore);
+            }
+
+
+
+
+
+
+        ctrl.findPrice = function() {
+          console.log(ctrl.itemID + ctrl.storeID);;
+          ctrl.newPrice = ItemService.getPrice(ctrl.itemID, ctrl.storeID);
+        }
+
+      ctrl.listTitle;
+      ctrl.addItem = function(itemName, storeID, listName){
+          ctrl.listTitle = listName;
+          ctrl.listItems.push({
+
+            "itemId" : ItemService.getIndex(itemName, storeID.storeName),
+            "itemName": itemName,
+            "itemPrice": storeID.itemPrice,
+            "barcode": 0,
+            "stores": {
+                  "storeName" : storeID.storeName
+            }
+
+        });
+
+
+        ctrl.item = {
+
+            "list": {
+                  "listId" : ctrl.list[$routeParams.index].listId
+            }
+            ,
+            "item": {
+                "itemId" : ItemService.getIndex(itemName, storeID.storeName)
+            }
+
+        }
+          //update the total
+
+          ctrl.getTotalPrice += storeID.itemPrice;
+          $http.post('http://localhost:8080/lists/additem',JSON.stringify(ctrl.item))
+                .then(function(response){
+                  if(response.data){
+                    console.log(response.data)
+
+                  }
+                  else {
+                    {
+                      console.log("Bad Info");
+                    }
+                  }
+                })
+
+      }
 
 
 }
